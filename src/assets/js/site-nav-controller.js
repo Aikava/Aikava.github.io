a793c81750642950d90e2bb19be237ea899d93d6
Ry Aikava <aikava.kizuna@gmail.com>
2024-10-26 17:26:30 +0300

    Added nav menu

--- src/assets/js/site-nav-controller.js
+++ src/assets/js/site-nav-controller.js
@@ -1,10 +1,28 @@
 const navPanel = document.querySelector(".site-nav-panel");
-let navPanelHidden = true;
 
 // add click catcher
-function showNavPanel() {
-  if (navPanel.classList.contains("out-screen"))
-    navPanel.classList.remove("out-screen");
-  else
-    navPanel.classList.add("out-screen");
+function showNavPanel(event) {
+  if (event && event.target === navPanel)
+    return;
+  const handleFadeIn = () => {
+    navPanel.classList.remove("nav-fade-in");
+    navPanel.removeEventListener("animationend", handleFadeIn);
+    document.body.addEventListener("click", showNavPanel);
+  };
+
+    const handleFadeOut = () => {
+      navPanel.classList.add("hidden");
+      navPanel.classList.remove("nav-fade-out");
+      navPanel.removeEventListener("animationend", handleFadeOut);
+      document.body.removeEventListener("click", showNavPanel);
+    };
+
+  if (navPanel.classList.contains("hidden")) {
+    navPanel.classList.add("nav-fade-in");
+    navPanel.classList.remove("hidden");
+    navPanel.addEventListener("animationend", handleFadeIn);
+  } else {
+    navPanel.classList.add("nav-fade-out");
+    navPanel.addEventListener("animationend", handleFadeOut);
+  }
 }
