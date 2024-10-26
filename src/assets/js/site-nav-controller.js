118a98b5660c25031c2f2b028f9f57360e1cc71d
Ry Aikava <aikava.kizuna@gmail.com>
2024-10-26 20:20:23 +0300

    added menu button animation

--- src/assets/js/site-nav-controller.js
+++ src/assets/js/site-nav-controller.js
@@ -1,27 +1,38 @@
 const navPanel = document.querySelector(".site-nav-panel");
+const navPanelButton = document.querySelector(".site-nav-button");
 
 // add click catcher
 function showNavPanel(event) {
   if (event && event.target === navPanel)
     return;
+
   const handleFadeIn = () => {
+    // navPanelButton.classList.remove("site-nav-button-close-animation");
+    
+    navPanelButton.classList.remove("nav-button-cross");
+    navPanelButton.classList.add("site-nav-close-button");
     navPanel.classList.remove("nav-fade-in");
     navPanel.removeEventListener("animationend", handleFadeIn);
     document.body.addEventListener("click", showNavPanel);
   };
 
-    const handleFadeOut = () => {
-      navPanel.classList.add("hidden");
-      navPanel.classList.remove("nav-fade-out");
-      navPanel.removeEventListener("animationend", handleFadeOut);
-      document.body.removeEventListener("click", showNavPanel);
-    };
+  const handleFadeOut = () => {
+    // navPanelButton.classList.remove("site-nav-button-menu-animation");
+    navPanelButton.classList.remove("site-nav-close-button");
+    navPanelButton.classList.remove("nav-button-menu");
+    navPanel.classList.add("hidden");
+    navPanel.classList.remove("nav-fade-out");
+    navPanel.removeEventListener("animationend", handleFadeOut);
+    document.body.removeEventListener("click", showNavPanel);
+  };
 
   if (navPanel.classList.contains("hidden")) {
+    navPanelButton.classList.add("nav-button-cross");
     navPanel.classList.add("nav-fade-in");
     navPanel.classList.remove("hidden");
     navPanel.addEventListener("animationend", handleFadeIn);
   } else {
+    navPanelButton.classList.add("nav-button-menu");
     navPanel.classList.add("nav-fade-out");
     navPanel.addEventListener("animationend", handleFadeOut);
   }
